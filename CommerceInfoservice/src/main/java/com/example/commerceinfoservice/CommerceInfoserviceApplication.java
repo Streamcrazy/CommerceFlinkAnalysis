2f757884e2abd779d26e0e7963a381c73d457d50
zht <996889303@qq.com>
2021-02-05 17:49:08 +0800

    update

---
+++ CommerceInfoservice/src/main/java/com/example/commerceinfoservice/CommerceInfoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerceinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceInfoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceInfoserviceApplication.class, args);
+    }
+
+}
---
+++ CommerceInfoservice/src/test/java/com/example/commerceinfoservice/CommerceInfoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerceinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceInfoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
